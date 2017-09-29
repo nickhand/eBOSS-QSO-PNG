ae0562cb69f174e7f007fd40d0b2105976844df0
Nick Hand <nhand@berkeley.edu>
2017-09-29 10:21:30 -0400

    add ez mock load functions

--- python/eboss_qso/measurements/__init__.py
+++ python/eboss_qso/measurements/__init__.py
@@ -8,13 +8,18 @@ results_dir = '/global/cscratch1/sd/nhand/Research/eBOSS/measurements'
 # allowed data versions
 DATA_VERSIONS = ['v1.6', 'v1.8', 'v1.9f']
 
+# EZmock subversions
+EZMOCK_VERSIONS = ['v1.8e']
+EZMOCK_SUBVERSIONS = ['reg', 'no', 'fph']
+
 # the fiducial DR12 cosmology
 _h = 0.676
 _Ob0 = 0.022/_h**2
 _Ocdm0 = 0.31 - _Ob0
 fidcosmo = Cosmology(h=_h, Omega_b=_Ob0, Omega_cdm=_Ocdm0, m_ncdm=None, T_cmb=2.7255)
 
-from .data import read_data, read_randoms
+from .data import read_data, read_randoms, finalize_data
+from .ezmock import read_ezmock_data, read_ezmock_randoms, finalize_ezmock
 from .results import save_data_spectra, save_RR_paircount
-from .utils import finalize_source, trim_redshift_range, redshift_range_type, get_hashkeys
+from .utils import trim_redshift_range, redshift_range_type, get_hashkeys
 from .zweights import fnl_weight, bias_weight
---
+++ python/eboss_qso/measurements/ezmock.py
@@ -0,0 +1,86 @@
+
+from . import data_dir, EZMOCK_VERSIONS, EZMOCK_SUBVERSIONS, fidcosmo
+import os
+from nbodykit.lab import CSVCatalog
+
+def read_ezmock_data(box, version, subversion, sample):
+    """
+    Read an eBOSS QSO EZ mock data file.
+
+    Parameters
+    ----------
+    box : int
+        the box number to load
+    version : str
+        the string specifying which version to load
+    subversion : str
+        the EZmock sub version to load, i.e., one of 'reg', 'no', 'fph'
+    sample : 'N' or 'S'
+        the sample to load
+    """
+    assert version in EZMOCK_VERSIONS
+    assert subversion in EZMOCK_SUBVERSIONS
+
+    sample = 'ngc' if sample == 'N' else 'sgc'
+
+    # get the file path
+    filename = f'zevoEZmock_QSO_{version}_veto_{sample}_{box:04d}_{subversion}.dat'
+    path = os.path.join(data_dir, 'mocks', '-'.join([version,subversion]), filename)
+
+    # load the source
+    names = ['RA', 'DEC', 'Z', 'WEIGHT_FKP', 'COMP', 'NZ', 'WEIGHT_COMP']
+    return CSVCatalog(path, names=names)
+
+
+def read_ezmock_randoms(version, sample):
+    """
+    Read a eBOSS QSO EZ mock randoms file.
+
+    Parameters
+    ----------
+    version : str
+        the string specifying which version to load
+    sample : 'N' or 'S'
+        the sample to load
+    """
+    assert version in EZMOCK_VERSIONS
+
+    sample = 'ngc' if sample == 'N' else 'sgc'
+
+    # get the file path
+    filename = f'randx100_QSO_{version}_veto_{sample}.dat'
+    path = os.path.join(data_dir, 'mocks', 'randoms', filename)
+
+    # load the source
+    names = ['RA', 'DEC', 'Z', 'WEIGHT_FKP', 'COMP', 'NZ', 'VETO']
+    return CSVCatalog(path, names=names)
+
+
+def finalize_ezmock(s, cosmo, P0_FKP=None):
+    """
+    Finalize the creation of a CatalogSource from a data file by
+    adding 'Position', 'Weight', and 'FKPWeight'.
+
+    Parameters
+    ----------
+    s : CatalogSource
+        the catalog source object
+    cosmo : Cosmology
+        the cosmology parameters
+    P0_FKP : float, optional
+        the P0 value to use to for FKPWeights
+    """
+    from nbodykit.transform import SkyToCartesian
+
+    # add the Position column
+    s['Position'] = SkyToCartesian(s['RA'], s['DEC'], s['Z'], cosmo, degrees=True)
+
+    # add systematic weights
+    if 'WEIGHT_COMP' in s:
+        s['Weight'] = s['WEIGHT_COMP']
+    else:
+        s['Weight'] = 1.0
+
+    # FKP WEIGHT
+    if P0_FKP is not None:
+        s['FKPWeight'] = 1. / (1 + s['NZ']*P0_FKP)
