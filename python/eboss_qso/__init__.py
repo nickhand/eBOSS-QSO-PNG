31239eea8ef715c57e7218bc53bce988549a70c7
Nick Hand <nhand@berkeley.edu>
2017-10-06 12:11:55 -0400

    fix bad environ var on NERSC

--- python/eboss_qso/__init__.py
+++ python/eboss_qso/__init__.py
@@ -1,6 +1,13 @@
 import os
+import socket
 
-# define some directories
-EBOSS_DIR = os.path.join(os.environ['THESIS_DIR'], 'eBOSS-QSO-PNG')
-EBOSS_FITS = os.path.join(EBOSS_DIR, 'fits')
-EBOSS_SPECTRA = os.path.join(EBOSS_DIR, 'measurements', 'spectra')
+# data and result directories
+if socket.gethostname() == 'utley':
+    # define some directories
+    EBOSS_DIR = os.path.join(os.environ['THESIS_DIR'], 'eBOSS-QSO-PNG')
+    EBOSS_FITS = os.path.join(EBOSS_DIR, 'fits')
+    EBOSS_SPECTRA = os.path.join(EBOSS_DIR, 'measurements', 'spectra')
+else:
+    EBOSS_DIR = None
+    EBOSS_FITS = None
+    EBOSS_SPECTRA = None
