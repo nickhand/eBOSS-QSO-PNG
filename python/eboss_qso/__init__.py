0b9e4b3ce15b93ce12b8bc57ae5b858e7ef9f21a
Nick Hand <nhand@berkeley.edu>
2017-11-25 04:28:32 -0500

    update paths

--- python/eboss_qso/__init__.py
+++ python/eboss_qso/__init__.py
@@ -1,13 +1,5 @@
 import os
-import socket
 
-# data and result directories
-if socket.gethostname() == 'utley':
-    # define some directories
-    EBOSS_DIR = os.path.join(os.environ['THESIS_DIR'], 'eBOSS-QSO-PNG')
-    EBOSS_FITS = os.path.join(EBOSS_DIR, 'fits')
-    EBOSS_SPECTRA = os.path.join(EBOSS_DIR, 'measurements', 'spectra')
-else:
-    EBOSS_DIR = None
-    EBOSS_FITS = None
-    EBOSS_SPECTRA = None
+EBOSS_DIR = os.environ['EBOSS_DIR']
+EBOSS_FITS = os.path.join(EBOSS_DIR, 'fits')
+EBOSS_SPECTRA = os.path.join(EBOSS_DIR, 'measurements', 'spectra')
