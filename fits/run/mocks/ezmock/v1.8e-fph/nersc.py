8db8807e406e1549bb0d402dba0089cdd445bef9
Nick Hand <nhand@berkeley.edu>
2017-11-25 14:28:02 -0500

    add kmax argument

--- fits/run/mocks/ezmock/v1.8e-fph/nersc.py
+++ fits/run/mocks/ezmock/v1.8e-fph/nersc.py
@@ -12,7 +12,7 @@ def main(ns):
         for box in tm.iterate(range(ns.start, ns.stop, ns.step)):
 
             RSDFitRunner.commands.clear()
-            add_commands(box=box, vary_shot_noise=vary_shot_noise, use_temp_files=True)
+            add_commands(box=box, vary_shot_noise=vary_shot_noise, kmax=ns.kmax, use_temp_files=True)
 
             command = RSDFitRunner.commands[ns.testno]
             QSOFitDriver.run_from_args(command.split()[1:], comm=tm.comm)
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     NERSCManager.add_argument('--stop', type=int, required=True)
     NERSCManager.add_argument('--step', type=int, default=1)
     NERSCManager.add_argument('--fix-shot-noise', action='store_true', default=False)
+    NERSCManager.add_argument('--kmax', type=float, default=0.3)
 
     ns = NERSCManager.parse_args()
     main(ns)
