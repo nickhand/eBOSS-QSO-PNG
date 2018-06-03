cd404215b063bedf715e7818bc87b4306fedb08b
Nick Hand <nhand@berkeley.edu>
2018-06-02 21:13:29 -0400

    fix bcast

--- python/eboss_qso/fits/nersc/job.template.sh
+++ python/eboss_qso/fits/nersc/job.template.sh
@@ -15,7 +15,7 @@ bcast-pip git+git://github.com/bccp/nbodykit.git
 
 # install correct nbodykit version to computing nodes
 bcast-pip .
-bcast $LOCAL_STACK/anaconda3/envs/pyrsd-anaconda-3.6-$NERSC_HOST.tar.gz
+bcast $PROGRAMS_DIR/pkgs/$NERSC_HOST/pyRSD*
 
 cd $SLURM_SUBMIT_DIR
 
