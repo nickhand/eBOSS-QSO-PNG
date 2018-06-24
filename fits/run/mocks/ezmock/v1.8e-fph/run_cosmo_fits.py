95d4f36b35dd0faf80a8054ab264f25055612b86
Nick Hand <nhand@berkeley.edu>
2018-06-24 14:10:14 -0400

    update fits to set p explicitly

--- fits/run/mocks/ezmock/v1.8e-fph/run_cosmo_fits.py
+++ fits/run/mocks/ezmock/v1.8e-fph/run_cosmo_fits.py
@@ -8,9 +8,14 @@ ZBINS = [(0.8, 2.2)]
 VERSION = 'v1.8e-fph'
 
 # hash values for different p
-HASHES = ['bba5aabfa6', '8ba79d78df', '0ef5c14a14']
-p = {'0ef5c14a14': 1.0, 'bba5aabfa6':None, '8ba79d78df':1.6}
-effective_redshifts = {None: 1.557, 1.0: 1.728, 1.6: 1.829}
+HASHES = [('bba5aabfa6', 1.0),
+          ('bba5aabfa6', 1.6),
+          ('8ba79d78df', 1.6),
+          ('0ef5c14a14', 1.0)
+          ]
+effective_redshifts = {'bba5aabfa6': 1.557,
+                       '0ef5c14a14': 1.728,
+                       '8ba79d78df': 1.829}
 
 # params we want to vary
 fixed_params = ['b1', 'sigma_fog', 'f', 'sigma8_z']
@@ -19,6 +24,9 @@ all_params = fixed_params + ['alpha_par', 'alpha_perp']
 @parametrize({'sample':['N', 'S'], 'hashstr':HASHES, 'params':[all_params, fixed_params]})
 def add_commands(sample, hashstr, params, box, vary_shot_noise=True, cov='analytic',
                     use_temp_files=False, overwrite=False, kmin=1e-4, kmax=0.3):
+
+    # unpack the tuple of hashstring
+    hashstr, p = hashstr
 
     # determine the params we are fitting
     if vary_shot_noise:
@@ -30,18 +38,19 @@ def add_commands(sample, hashstr, params, box, vary_shot_noise=True, cov='analyt
     filename = os.path.join(dirname, f'poles_zevoEZmock_{VERSION}_QSO-{sample}_{box:04d}-{hashstr}.json')
 
     # the effective redshift
-    zeff = effective_redshifts[p[hashstr]]
+    zeff = effective_redshifts[hashstr]
 
     # make the command
     command = f"eboss-qso-fit nlopt -f {filename} --vary {params} --stats P0 P2"
-    command += f" -i {ITERATIONS} --kmin {kmin} --kmax {kmax} --cov {cov} --zeff {zeff}"
+    command += f" -i {ITERATIONS} --kmin {kmin} --kmax {kmax} --cov {cov} --zeff {zeff} -p {p}"
     if use_temp_files:
         command += " --use-temp-files"
     if overwrite:
         command += " --overwrite"
 
     # and register it
-    tag = {'sample':sample, 'p':p[hashstr], 'params':params, 'zbin':ZBINS[0]}
+    weighted = hashstr != "bba5aabfa6"
+    tag = {'sample':sample, 'p':p, 'weighted':weighted, 'params':params, 'zbin':ZBINS[0]}
     RSDFitRunner.register(command, tag=tag)
 
 
