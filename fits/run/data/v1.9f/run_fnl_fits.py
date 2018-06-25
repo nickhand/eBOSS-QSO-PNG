36c6e7a897dd393467d6f3341647b171d1ae57e3
Nick Hand <nhand@berkeley.edu>
2018-06-24 23:04:08 -0400

    update data running scripts

--- fits/run/data/v1.9f/run_fnl_fits.py
+++ fits/run/data/v1.9f/run_fnl_fits.py
@@ -7,13 +7,23 @@ NWALKERS = 50
 ITERATIONS = 500
 ZBINS = [(0.8, 2.2)]
 VERSION = 'v1.9f'
-HASHES = ['bba5aabfa6', '8ba79d78df', '0ef5c14a14']
-p = {'0ef5c14a14': 1.0, 'bba5aabfa6':None, '8ba79d78df':1.6}
 
+# hash values for different p
+HASHES = [('bba5aabfa6', 1.0),
+          ('bba5aabfa6', 1.6),
+          ('8ba79d78df', 1.6),
+          ('0ef5c14a14', 1.0)
+          ]
+effective_redshifts = {'bba5aabfa6': 1.557,
+                       '0ef5c14a14': 1.728,
+                       '8ba79d78df': 1.829}
 
 @parametrize({'sample':['N', 'S'], 'hashstr':HASHES})
 def add_commands(sample, hashstr, vary_shot_noise=True, cov='analytic',
                     use_temp_files=False, overwrite=False, kmin=1e-4, kmax=0.3):
+
+    # unpack the tuple of hashstring
+    hashstr, p = hashstr
 
     # determine the params we are fitting
     all_params = ['b1', 'sigma_fog', 'f_nl']
@@ -24,16 +34,20 @@ def add_commands(sample, hashstr, vary_shot_noise=True, cov='analytic',
     dirname = os.path.join(EBOSS_SPECTRA, 'data', VERSION)
     filename = os.path.join(dirname, f'poles_eboss_{VERSION}-focal-QSO-{sample}-{hashstr}.json')
 
+    # the effective redshift
+    zeff = effective_redshifts[hashstr]
+
     # make the command
     command = f"eboss-qso-fit mcmc -f {filename} --vary {params} --stats P0 P2"
-    command += f" -i {ITERATIONS} -w {NWALKERS} --kmin {kmin} --kmax {kmax} --cov {cov}"
+    command += f" -i {ITERATIONS} -w {NWALKERS} --kmin {kmin} --kmax {kmax} --cov {cov} --zeff {zeff} -p {p}"
     if use_temp_files:
         command += " --use-temp-files"
     if overwrite:
         command += " --overwrite"
 
     # and register it
-    tag = {'sample':sample, 'p':p[hashstr], 'zbin':ZBINS[0]}
+    weighted = hashstr != "bba5aabfa6"
+    tag = {'sample':sample, 'p':p, 'weighted':weighted, 'zbin':ZBINS[0]}
     RSDFitRunner.register(command, tag=tag)
 
 
