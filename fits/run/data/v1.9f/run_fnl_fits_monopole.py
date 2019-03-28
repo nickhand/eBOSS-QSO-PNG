58e4044b3d237d725d438e1d1c9434e862fac4f1
Nick Hand <nicholas.adam.hand@gmail.com>
2019-03-27 21:03:32 -0400

    files to run monopole only fits

---
+++ fits/run/data/v1.9f/run_fnl_fits_monopole.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+from eboss_qso.fits import RSDFitRunner, parametrize
+from eboss_qso import EBOSS_SPECTRA
+import os
+
+NWALKERS = 50
+ITERATIONS = 500
+ZBINS = [(0.8, 2.2)]
+VERSION = 'v1.9f'
+
+# data and theory values for p
+p_values = [(None, 1.0),
+            (None, 1.6),
+            (1.0, 1.0),
+            (1.6, 1.6)]
+
+# effective redshifts based on data p value
+effective_redshifts = {None: (1.557, 1.557),
+                       1.0: (1.728, 1.671),
+                       1.6: (1.829, 1.794)}
+
+
+@parametrize({'sample': ['N', 'S'], 'p_values': p_values})
+def add_commands(sample, p_values, vary_shot_noise=True, cov='analytic',
+                 use_temp_files=False, overwrite=False, kmin=1e-4, kmax=0.3):
+
+    # the p values to use
+    data_p, theory_p = p_values
+
+    # determine the params we are fitting
+    all_params = ['b1', 'sigma_fog', 'f_nl']
+    if vary_shot_noise:
+        all_params += ['N']
+    params = " ".join(all_params)
+
+    # the effective redshift
+    z_eff = effective_redshifts[data_p]
+
+    # arguments for reading the data
+    command = "eboss-qso-fit mcmc"
+    command += f" --kind data --version {VERSION} --sample {sample} --ells 0"
+    command += f" --z_eff {z_eff[0]}"
+    command += f" --theory_p {theory_p} --kmin {kmin} --kmax {kmax}"
+    if data_p is not None:
+        command += f" --data_p {data_p}"
+
+    # theory arguments
+    command += f" --vary {params} -i {ITERATIONS} -w {NWALKERS} --cov {cov}"
+    if use_temp_files:
+        command += " --use-temp-files"
+    if overwrite:
+        command += " --overwrite"
+
+    # and register it
+    weighted = data_p is not None
+    tag = {'sample': sample, 'p': theory_p,
+           'weighted': weighted, 'zbin': ZBINS[0]}
+    RSDFitRunner.register(command, tag=tag)
+
+
+if __name__ == '__main__':
+
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('--vary-shot-noise',
+                        choices=[0, 1], type=int, required=True)
+    parser.add_argument('--cov', choices=['mock', 'analytic'], required=True)
+    parser.add_argument('--overwrite', action='store_true', default=True)
+    parser.add_argument('--kmin', type=float, default=1e-4)
+    parser.add_argument('--kmax', type=float, default=0.3)
+
+    ns, args = parser.parse_known_args()
+
+    add_commands(**vars(ns))
+    RSDFitRunner.execute(args=args)
---
+++ fits/run/data/v1.9f/run_joint_fnl_fits_monopole.py
@@ -0,0 +1,61 @@
+from eboss_qso.fits.joint import run_joint_mcmc_fit
+from eboss_qso.measurements.utils import make_hash
+import os.path as osp
+import os
+from glob import glob
+
+
+ARGS = [(False, 1.0),
+        (False, 1.6),
+        (True, 1.6),
+        (True, 1.0)
+        ]
+ITERATIONS = 500
+WALKERS = 100
+
+
+def main(argnum, kmin):
+
+    z_weighted, p = ARGS[argnum]
+
+    # the data to load
+    kws = {}
+    kws['version'] = 'v1.9f'
+    kws['krange'] = '%s-0.3' % kmin
+    kws['params'] = 'basemodel-N-fnl'
+    kws['zrange'] = '0.8-2.2'
+    kws['z_weighted'] = z_weighted
+    kws['p'] = p
+    kws['ells'] = [0]
+
+    hashstr = make_hash(kws)
+
+    # output directory
+    output = osp.join(os.environ['EBOSS_FITS'], 'data')
+    output = osp.join(output, kws['version'],
+                      kws['krange'], kws['params'], kws['zrange'])
+    output = osp.join(output, 'QSO-N+S-%s' % hashstr)
+
+    if not osp.exists(output):
+        os.makedirs(output)
+
+    # output file name
+    i = len(glob(osp.join(output, '*npz')))
+    output = osp.join(output, 'chain_%dx%d_%d.npz' % (ITERATIONS, WALKERS, i))
+    print(output)
+
+    # run
+    run_joint_mcmc_fit('data', ITERATIONS, WALKERS,
+                       output, kws, joint_params=['f_nl'])
+
+
+if __name__ == '__main__':
+
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("argnum", type=int, choices=[0, 1, 2, 3])
+    parser.add_argument('kmin', type=str, choices=["0.0001", "0.005"])
+
+    ns = parser.parse_args()
+    main(ns.argnum, ns.kmin)
